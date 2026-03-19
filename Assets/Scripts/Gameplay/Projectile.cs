696ac0cf4a47469ed262a68dbb23d3dfeb2e306f
MrNoc7 <andysantana854@gmail.com>
2026-03-19 14:47:07 +0100

    feat: ShootHandler function
    Handles the shooting direction and speed

---
+++ Assets/Scripts/Gameplay/Projectile.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody rb; // Reference to the Rigidbody component of the projectile
+
+    [Header("Bullet Stats")]
+    [Space]
+    [Tooltip("Bullet Speed")]
+    [SerializeField] private float speed = 10f;
+    [Tooltip("Bullet Damage ")]
+    [SerializeField] private float damage = 1f;
+    [Tooltip("Bullet's lifetime")]
+    [SerializeField] private float lifetime = 3f;  
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Start()
+    {
+        ShootHandler(transform.forward, speed);
+
+        
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    
+    /// <summary>
+    /// CurrentDirection can be "transform.forward" or any direction you want to shoot the projectile towards, and speed is the speed of the projectile.
+    /// </summary>
+    /// <param name="CurrentDirection"></param>
+    /// <param name="speed"></param>
+    private void ShootHandler(Vector3 CurrentDirection, float speed) 
+    {
+        rb.AddForce(CurrentDirection * speed); //CurrentDirection is the transform
+       
+
+
+
+
+
+    }
+
+}
+
