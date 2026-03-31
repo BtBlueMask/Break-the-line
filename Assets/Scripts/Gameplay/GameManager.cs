af061d58ad8fadb81bdaf8a4e1ad004d0dbd8b0d
MrNoc7 <andysantana854@gmail.com>
2026-03-31 15:55:16 +0200

    chore: Spawn Ennemies

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -15,6 +15,7 @@ public class GameManager : MonoBehaviour
     [Header("Keeps track of")]
     [Space]
     [SerializeField] private int _currentScore;
+    [SerializeField] private int _highScore;
     [SerializeField] private int _currentRound;
     [Tooltip("Do not change maxRounds(3) unless it's scaleable")]
     [SerializeField] private int _maxRounds = 3;
@@ -46,22 +47,31 @@ public class GameManager : MonoBehaviour
     /// </summary>
     /// <param name="scoreToAdd">This parameter is used to add score to the _currentScore</param>
 
-    #region Score save/load/update
+    #region Score save/load/update/reset
     private void SaveScore()
     {
-        PlayerPrefs.SetInt("HighScore", _currentScore);
-        PlayerPrefs.Save();
+        if (_currentScore > _highScore)
+        {
+            _highScore = _currentScore;
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            PlayerPrefs.Save();
+        }
     }
-
     private void LoadScore()
     {
-        _currentScore = PlayerPrefs.GetInt("HighScore", 0);
+        _highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
     private void UpdateScore(int scoreToAdd)
     {
         _currentScore += scoreToAdd;
         Debug.Log("Score: " + _currentScore);
     }
+    private void ResetRoundAndScore()
+    {
+        _currentScore = 0;
+        _currentRound = 0;
+    }
+
     #endregion
 
     /// <summary>
@@ -69,32 +79,48 @@ public class GameManager : MonoBehaviour
     /// </summary>
     private void RestartGame()
     {
+        SaveScore();
+        LoadScore();
         MainScene();
         ResetRoundAndScore();
         Debug.Log("Game Reset");
     }
 
-    private void ResetRoundAndScore()
-    {
-        _currentScore = 0;
-        _currentRound = 0;
-    }
+    #region Enemy Spawning
 
+    private void SpawnEnemies()
+    {
+        if (_currentRound < _maxRounds)
+        {
+            Wave currentWave = _waves[_currentRound];
+            SpawnWave(currentWave);
+            UpdateRoundNumber();
+        }
+        else
+        {
+            Debug.Log("Max rounds reached. Ending game.");
+            EndScene();
+        }
+    }
     private void SpawnEnemy(GameObject currentEnemy)
     {
-        Instantiate(currentEnemy); 
+        Instantiate(currentEnemy);
         Debug.Log("Enemy Spawned" + currentEnemy.name);
     }
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="currentwave">Local variable of the wave script</param>
     private void SpawnWave(Wave currentwave)
     {
         List<GameObject> enemiesToSpawn = currentwave.enemies;
 
-        foreach (GameObject enemy in currentwave.enemies)
+        foreach (GameObject enemy in enemiesToSpawn)
         {
             SpawnEnemy(enemy);
         }
     }
+    #endregion
 
     #region Scenes
     private void MenuScene()
@@ -104,7 +130,7 @@ public class GameManager : MonoBehaviour
 
     private void MainScene()
     {
-       SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("MainScene");
     }
 
     private void EndScene()
