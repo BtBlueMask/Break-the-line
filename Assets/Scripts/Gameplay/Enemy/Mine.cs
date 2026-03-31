9ee0e6b1683e226d40f277d61408841c1e33b3a5
GuusWijnker2067 <guuswijnker206@gmail.com>
2026-03-31 15:56:42 +0200

    Added a script and placeholder for minelayer and mine

---
+++ Assets/Scripts/Gameplay/Enemy/Mine.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Mine : BaseEnemy
+{
+    [SerializeField] private SphereCollider thisCollider;
+
+    [SerializeField] private float moveSpeed;
+    private float directionDeviation;
+
+    [SerializeField] private float blastRadius;
+
+    override protected void Start()
+    {
+        base.Start();
+
+        directionDeviation = Random.Range(-0.5f, 0.5f);
+    }
+
+    protected override void OnUpdate()
+    {
+        transform.position += new Vector3(directionDeviation, 0, 1) * moveSpeed * Time.deltaTime;
+    }
+
+    protected override void OnPlayerHit(Player player)
+    {
+        Debug.Log("The player was hit");
+        MineExplode();
+        OnDeath();
+    }
+
+    protected override void OnProjectiletHit(Projectile projectile)
+    {
+        MineExplode();
+        OnDeath();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, blastRadius);
+    }
+
+    private void MineExplode()
+    {
+        thisCollider.radius = blastRadius;
+    }
+}
