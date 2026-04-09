d5bcc62cbca10fdd39c521777c532dd4272ac7d9
Karsten <btbluemask@gmail.com>
2026-04-09 13:34:54 +0200

    [+] made particle manager

--- AGENTS.md
+++ AGENTS.md
@@ -1,8 +1,4 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: Break the line
 - Unity version: Unity 6000.3.4f1
-- Active game object:
-  - Name: GameManager
-  - Tag: Untagged
-  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/General/ParticleManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Spark;
+    [SerializeField] private GameObject Fire;
+    [SerializeField] private GameObject Flash;
+    [SerializeField] private GameObject Smoke;
+
+
+    public void StartSpark(Vector3 StartPos)
+    {
+        Instantiate(Spark, StartPos, Quaternion.Euler(0,0,0));
+    }
+
+    public void StartFire(Vector3 StartPos)
+    {
+        Instantiate(Fire, StartPos, Quaternion.Euler(0, 0, 0));
+    }
+
+
+
+}
